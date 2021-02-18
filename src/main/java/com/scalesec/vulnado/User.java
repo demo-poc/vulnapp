6397f9372fb8ef142881135bd62f2c47ca265942
Rahul Gunasekaran <71350204+rahulguna10@users.noreply.github.com>
2021-02-18 14:20:51 -0500

    Change in User.java

--- src/main/java/com/scalesec/vulnado/User.java
+++ src/main/java/com/scalesec/vulnado/User.java
@@ -15,6 +15,7 @@ public class User {
   public User(String id, String username, String hashedPassword) {
     this.id = id;
     this.username = username;
+    this.username = username;
     this.hashedPassword = hashedPassword;
   }
 
