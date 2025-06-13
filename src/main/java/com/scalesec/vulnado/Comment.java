1ce9124405f54534a9900feca476e9886f1a04fa
Sneha Kokil <snehanarendra@gmail.com>
2025-06-13 08:52:59 -0400

    testing

--- src/main/java/com/scalesec/vulnado/Comment.java
+++ src/main/java/com/scalesec/vulnado/Comment.java
@@ -39,7 +39,7 @@ public class Comment {
     try {
       Connection cxn = Postgres.connection();
       stmt = cxn.createStatement();
-
+      System.out.println("testing attestation attempt 1");
       String query = "select * from comments;";
       ResultSet rs = stmt.executeQuery(query);
       while (rs.next()) {
